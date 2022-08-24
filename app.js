f965fa5d7d9933b7012335fdfc0bff1a44d703c8
vihan pamudya <vihanpamudya12@gmail.com>
2022-08-25 03:02:54 +0530

    configuration

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 import { Express } from "express";
+import mongoose from "mongoose";
+
+const app = Express();
+
+// Conection to mongodb
+mongoose.connect("mongodb://localhost/toDoApp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middlewares
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.static("public"));
+app.set("view engine", "ejs");
+
+//Routes
+
+// Server Configuration
+app.listen(3000, () => console.log("Server runing on port: 3000"));
