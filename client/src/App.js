ff440bb0251876751723454bf4c0c274cc1c4b70
vihan pamudya <vihanpamudya12@gmail.com>
2022-08-25 23:44:11 +0530

    client chage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import {useState,useEffect} from "react";
 
 function App() {
+
+const [todos,setTodos] = useState([])
+const [popupActive, setPopupActive] = useState(false)
+const [newTodo, setNewTodo]= useState("")
+
   return (
     <div className="App">
-      
+      <h1>Welcome, ToDo</h1>
+      <h4>Your Tasks</h4>
+
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">Get the bread</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
     </div>
   );
 }
