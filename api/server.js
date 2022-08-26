79900fcdce23e4f2d8b69382dd664ec1bb981505
vihan pamudya <vihanpamudya12@gmail.com>
2022-08-26 10:42:37 +0530

    api completed

--- api/server.js
+++ api/server.js
@@ -31,6 +31,31 @@ app.get("/todos",async(req,res)=>{
     res.json(todos); 
 })
 
+app.post("/todo/new",async(req,res)=>{
+    const todo = new Todo({
+        text:req.body.text
+    })
+
+    todo.save();
+    res.json(todo)
+})
+
+app.delete("/todo/delete/:id",async(req,res)=>{
+    const id = req.params.id
+    const result = await Todo.findByIdAndDelete(id)
+
+    res.json(result)
+
+})
+
+app.put("/todo/complete/:id",async(req,res)=>{
+    const todo = await Todo.findById(req.params.id)
+    todo.complete = !todo.complete
+
+    todo.save()
+    res.json(todo)
+})
+
 app.listen(3000, ()=>{
     console.log("Server started on port : 3000")
 })
